23273d54fc281e79107762413e00fe938d241588
flawlesscode254 <denzme414@gmail.com>
2022-05-26 20:28:03 +0300

    started working on it

---
+++ app/src/main/java/com/example/sharingapp/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.example.sharingapp.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
